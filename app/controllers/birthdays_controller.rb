3077d5bd69fcd5ad30b62ea96eb495c321564bee
Starlight Romero <starlightromero@gmail.com>
2017-06-06 23:04:53 -0400

    separated user lookup from 'their' lookup

--- app/controllers/birthdays_controller.rb
+++ app/controllers/birthdays_controller.rb
@@ -12,9 +12,11 @@ class BirthdaysController < ApplicationController
     @birth_card = @birthday.birth_card
     if @birthday == @member.birthday
       @pronoun = "Your"
+      @querent = "member"
       @birthday.zodiac_sign = @member&.zodiac_sign&.intern
     else
       @pronoun = "Their"
+      @querent = "guest"
     end
     if @birthday.astrological_sign.is_cusp?
       @personality_card = nil
